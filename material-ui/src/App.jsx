7a5eeab0e654da4d5351abdeb80e9379ae9f9834
arielZarate <arieltecnico@gmail.com>
2023-06-22 19:19:04 -0300

    add proyect new

--- material-ui/src/App.jsx
+++ material-ui/src/App.jsx
@@ -1,46 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
 
-import Drawer_basico from './componentes/Dashboard/Drawer_basico'
-import Drawer2 from './componentes/NavBar/Drawer2'
-import ShowAlert from './componentes/Alertas/Alert'
-
-import NavBar from './componentes/Drawer/NavBar2'
+import Drawer_basico from "./componentes/Dashboard/Drawer_basico";
+import Drawer2 from "./componentes/NavBar/Drawer2";
+import ShowAlert from "./componentes/Alertas/Alert";
+import NavbarBase from "./componentes/NavBar2/NavBarBase";
+import NavBar from "./componentes/Drawer/NavBar2";
 function App() {
- 
-
   return (
     <div className="App">
-      <h3>Componentes de react</h3>  
- {/*     <Botones/>  */}
-    {/*<Tipografias/> */}
-    {/* <Caja/> */}
-{/*     <CrearTemas/> */}
- 
-  {/*   <Grilla/> */}
-{/*     <PracticaCard/> */}
-  {/*   <MyCard/> */}
-{/*  <NavBar/>   */}
-   {/* <NavListDrawer/>  */}
-     {/* <MostrarListDrawer/>  */}
-   {/*  <NavBar2/> */}
+      <h3>Componentes de react</h3>
+      {/*     <Botones/>  */}
+      {/*<Tipografias/> */}
+      {/* <Caja/> */}
+      {/*     <CrearTemas/> */}
 
- {/*   <Header/> */}
+      {/*   <Grilla/> */}
+      {/*     <PracticaCard/> */}
+      {/*   <MyCard/> */}
+      {/*  <NavBar/>   */}
+      {/* <NavListDrawer/>  */}
+      {/* <MostrarListDrawer/>  */}
 
-    {/*   <BotonesYIconos/>  */}
+      <NavbarBase />
 
- {/*    <Dashboard/> */}
-{/*     <Drawer_basico/> */}
+      {/*   <Header/> */}
 
-{/* 
- <Drawer2/> */}
+      {/*   <BotonesYIconos/>  */}
 
+      {/*    <Dashboard/> */}
+      {/*   <Drawer_basico/>  */}
+      {/*  <Drawer2 /> */}
 
- <ShowAlert/> 
+      {/*  <ShowAlert/>  */}
     </div>
-
-  )
+  );
 }
 
-export default App
+export default App;
